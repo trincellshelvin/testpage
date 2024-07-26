099064f451f735d320590cc14c0d98f262865209
Trincell Shelvin <trincellshelvin@gmail.com>
2024-07-26 15:40:42 -0500

    practive on main js from mdn

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,23 @@ myImage.onclick = ()=> {
     }
 
 };
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("hi");
+
+function setUserName(){
+    const myName = prompt("Please enter your name");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
+
+if (!localStorage.getItem("name")){
+    setUserName();
+
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+myButton.onclick = () => {
+    setUserName();
+}
